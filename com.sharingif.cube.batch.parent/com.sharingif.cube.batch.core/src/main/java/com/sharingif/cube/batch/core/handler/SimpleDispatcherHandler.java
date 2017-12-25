6192505ce89a4b273cb204b20d21a4f4abfafbfe
JolyHuang <JolyEmail@Gmail.COM>
2017-12-25 17:11:58 +0800

    init

--- com.sharingif.cube.batch.parent/com.sharingif.cube.batch.core/src/main/java/com/sharingif/cube/batch/core/handler/SimpleDispatcherHandler.java
+++ com.sharingif.cube.batch.parent/com.sharingif.cube.batch.core/src/main/java/com/sharingif/cube/batch/core/handler/SimpleDispatcherHandler.java
@@ -3,11 +3,16 @@ package com.sharingif.cube.batch.core.handler;
 import com.sharingif.cube.batch.core.request.JobRequest;
 import com.sharingif.cube.batch.core.request.JobRequestContextResolver;
 import com.sharingif.cube.communication.handler.AbstractDispatcherHandler;
+import com.sharingif.cube.communication.view.View;
+import com.sharingif.cube.communication.view.exception.NoViewFoundException;
+import com.sharingif.cube.communication.view.exception.ViewException;
+import com.sharingif.cube.core.exception.handler.ExceptionContent;
 import com.sharingif.cube.core.handler.adapter.HandlerAdapter;
 import com.sharingif.cube.core.handler.adapter.MultiHandlerMethodAdapter;
 import com.sharingif.cube.core.handler.mapping.HandlerMapping;
 import com.sharingif.cube.core.handler.mapping.MultiHandlerMapping;
 import com.sharingif.cube.core.handler.mapping.RequestMappingHandlerMapping;
+import com.sharingif.cube.core.request.RequestContext;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
@@ -28,25 +33,4 @@ import java.util.List;
  */
 public class SimpleDispatcherHandler extends AbstractDispatcherHandler<JobRequest> {
 
-    private DataSourceTransactionManager dataSourceTransactionManager;
-
-    public DataSourceTransactionManager getDataSourceTransactionManager() {
-        return dataSourceTransactionManager;
-    }
-
-    public void setDataSourceTransactionManager(DataSourceTransactionManager dataSourceTransactionManager) {
-        this.dataSourceTransactionManager = dataSourceTransactionManager;
-    }
-
-    @Override
-    public void doDispatch(JobRequest request) {
-        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
-        def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
-        def.setIsolationLevel(DefaultTransactionDefinition.ISOLATION_REPEATABLE_READ);
-        TransactionStatus status = dataSourceTransactionManager.getTransaction(def);
-
-        super.doDispatch(request);
-
-        dataSourceTransactionManager.commit(status);
-    }
 }
